9eae0715092f25fdaf93c507c62542c4e8e05bf3
Caspar Harmer <rapsac@gmail.com>
2017-09-14 19:48:19 +1200

    Create DualSelectController.js

---
+++ src/aura/DualSelect/DualSelectController.js
@@ -0,0 +1,8 @@
+({
+    moveLeftToRight : function(component,event, helper){
+        helper.moveItems(component,'left');
+    },
+    moveRightToLeft : function(component,event, helper){
+        helper.moveItems(component,'right');
+    },
+})
